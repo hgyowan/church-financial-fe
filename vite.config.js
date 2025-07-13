4181f9ca3afb7e50f6e42324e4b62534d6ed9055
hgyowan <ghkdrydhsk@gmail.com>
2025-07-13 16:28:53 +0900

    feat: default port 변겨

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080, // 원하는 포트 번호로 변경 가능
+    open: true, // 브라우저 자동 열기
+  },
 })
